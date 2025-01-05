38c48378665806007a42c2560d4111fd931ef2fb
Arturo Martinez <gerencia@climatecontrolsing.com>
2025-01-04 22:05:27 -0500

    test28

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -8,7 +8,7 @@ export const loginProduct = async (req: Request, res: Response) => {
     console.log(`into loginProduct ${owner}, ${clav_prodct}`)
     const loginProduct = await loginProductInDb(owner, clav_prodct);
     console.log(`output loginProduct ${loginProduct}`)
-    res.status(201).json(loginProduct);
+    res.status(200).json(loginProduct);
   } catch (error) {
     res.status(500).send('Error find product');
   }
@@ -20,7 +20,7 @@ export const findProduct = async (req: Request, res: Response) => {
     console.log(`into findProduct ${id}`)
     const findProduct = await findProductInDb(id);
     console.log(`output findProduct ${findProduct}`)
-    res.status(201).json(findProduct);
+    res.status(200).json(findProduct);
   } catch (error) {
     res.status(404).send('Error find product');
   }
